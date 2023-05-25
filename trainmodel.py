ce7472f44e553731816dd29c13c29f97af987f86
superbaby81230 <superbaby81230@gmail.com>
2023-05-25 08:09:08 -0700

    Modified modules

--- trainmodel.py
+++ trainmodel.py
@@ -1,10 +1,10 @@
-from llama_index import SimpleDirectoryReader, GPTListIndex, GPTVectorStoreIndex, LLMPredictor, PromptHelper
+from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
 from langchain.chat_models import ChatOpenAI
 import gradio as gr
 import sys
 import os
 
-os.environ["OPENAI_API_KEY"] = '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'
+os.environ["OPENAI_API_KEY"] = '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'
 
 def construct_index(directory_path):
     max_input_size = 4096
@@ -14,18 +14,39 @@ def construct_index(directory_path):
 
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
 
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-4", max_tokens=num_outputs))
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
 
     documents = SimpleDirectoryReader(directory_path).load_data()
 
-    index = GPTVectorStoreIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
 
     index.save_to_disk('index.json')
 
     return index
 
+# def construct_index(directory_path):
+#     max_input_size = 4096
+#     num_outputs = 256
+#     max_chunk_overlap = 20
+#     chunk_size_limit = 600
+
+#     llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003",
+#     max_tokens=num_outputs))
+#     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap,
+#     chunk_size_limit=chunk_size_limit)
+
+#     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor,
+#     prompt_helper=prompt_helper)
+
+#     documents = SimpleDirectoryReader(directory_path).load_data()
+
+#     index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+
+#     index.save_to_disk('index.json')
+#     return index
+
 def chatbot(input_text):
-    index = GPTVectorStoreIndex.load_from_disk('index.json')
+    index = GPTSimpleVectorIndex.load_from_disk('index.json')
     response = index.query(input_text, response_mode="compact")
     return response.response
 
